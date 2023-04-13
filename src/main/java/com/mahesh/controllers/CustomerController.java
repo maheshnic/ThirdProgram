f39fde23a21fc24c65568c222937227dbd49d0e6
maheshnic <msujawal@gmail.com>
2023-04-13 15:14:58 +0530

    Changes related to Spring JPA

--- src/main/java/com/mahesh/controllers/CustomerController.java
+++ src/main/java/com/mahesh/controllers/CustomerController.java
@@ -1,16 +1,25 @@
 package com.mahesh.controllers;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.mahesh.dto.CustomerDTO;
 import com.mahesh.exception.NotFoundException;
 import com.mahesh.service.CustomerService;
+
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
--- src/main/java/com/mahesh/exception/CustomErrorController.java
+++ src/main/java/com/mahesh/exception/CustomErrorController.java
@@ -1,16 +1,17 @@
 package com.mahesh.exception;
 
-import jakarta.validation.ConstraintViolationException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import jakarta.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class CustomErrorController {
